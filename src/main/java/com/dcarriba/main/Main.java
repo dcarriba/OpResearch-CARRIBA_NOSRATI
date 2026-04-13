fb06e85f76c08b3591e8771bc2aa9d4fc11a7d50
Daniel Carriba <dcarriba@gmx.net>
2026-04-13 17:06:29 +0200

    main package

---
+++ src/main/java/com/dcarriba/main/Main.java
@@ -0,0 +1,17 @@
+package com.dcarriba.main;
+
+/**
+ * The {@link Main} class of the project.
+ */
+public class Main {
+
+
+    /**
+     * Main function of the project
+     *
+     * @param args arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
